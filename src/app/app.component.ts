f72b477b6dc9eb671f3dd6044c9419077ec60c65
Rohit Kuner <Rohit.Kuner.-ND@disney.com>
2023-11-30 08:44:06 +0530

    Basic Customer Application using Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'myapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export  class AppComponent {
+  title = 'CustomerApplication';
+}
+
+@Component({})
+export class AppComponent1 {
+  title = 'CustomerApplication';
+}
